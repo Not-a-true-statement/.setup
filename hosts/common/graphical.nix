60dfbf9eab8be380c1a8c3770e4d99369a8e7238
user <email@generic.com>
2025-02-14 08:01:05 +0000

    Move nvidia to desktop only

--- hosts/common/graphical.nix
+++ hosts/common/graphical.nix
@@ -3,40 +3,6 @@
 {
   system = { pkgs, user, config, ... }: {
 
-    # Enable OpenGL
-    hardware.graphics = { enable = true; };
-
-    # Load nvidia driver for Xorg and Wayland
-    services.xserver.videoDrivers = [ "nvidia" ];
-
-    hardware.nvidia = {
-
-      # Modesetting is required.
-      modesetting.enable = true;
-
-      # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-      # Enable this if you have graphical corruption issues or application crashes after waking
-      # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
-      # of just the bare essentials.
-      powerManagement.enable = false;
-
-      # Fine-grained power management. Turns off GPU when not in use.
-      # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-      powerManagement.finegrained = false;
-
-      # Use the NVidia open source kernel module (not to be confused with the
-      # supported GPUs is at: 
-      # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
-      open = false;
-
-      # Enable the Nvidia settings menu,
-      # accessible via `nvidia-settings`.
-      nvidiaSettings = true;
-
-      # Optionally, you may need to select the appropriate driver version for your specific GPU.
-      package = config.boot.kernelPackages.nvidiaPackages.stable;
-    };
-
     services.xserver.enable = true;
     services.xserver.displayManager = {
       session = [
